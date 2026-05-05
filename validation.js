98a7cf961140abc111b8cf65422f946a1d800ec3
Louis Gian R. Balino <louiszom24@gmail.com>
2026-05-05 23:57:03 +0800

    Hirappp

--- validation.js
+++ validation.js
@@ -3,6 +3,7 @@ const firstname_input = document.getElementById('firstname-input')
 const email_input = document.getElementById('email-input')
 const password_input = document.getElementById('password-input')
 const repeat_password_input = document.getElementById('repeat-password-input')
+const error_message = document.getElementById('error-message')
 
 form.addEventListener('submit', (e) => {
     //e.preventDefault()
@@ -15,4 +16,41 @@ form.addEventListener('submit', (e) => {
     else{
         errors = getLoginFormErrors(email_input.value, password_input.value)
     }
+
+    if(errors.length > 0){
+        e.preventDefault()
+        error_message.innerText = error.join(". ")
+    }
+})
+
+function getSignupFormErrors(firstname, email, password, repeatPassword){
+    let erorrs = []
+
+    if(firstname === '' || firstname == null){
+        errors.push('Firstname is required')
+        firstname_input.parentElement.classList.add('incorrect')
+    }
+    if(email === '' || email == null){
+        errors.push('Email is required')
+        email_input.parentElement.classList.add('incorrect')
+    }
+    if(password === '' || password == null){
+        errors.push('Password is required')
+        password_input.parentElement.classList.add('incorrect')
+    }
+    if(password !== repeatPassword){
+        errors.push('Password does not match repeated password')
+    }
+
+    return errors;
+}
+const allInputs = [firstname_input, email_input, password_input, repeat_password_input]
+
+allInputs.forEach(input => {
+    input.addEventListener('input', () => {
+        if(input.parentElement.classList.contains('incorrect')){
+            input.parentElement.classList.remove('incorrect')
+            error_message.innerText = ''
+        }
+    })
 })
